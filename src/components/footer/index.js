2f621e68b48bfff9af2e843b614d4ea77842e14d
Phillip48 <phillippereira02@gmail.com>
2022-06-13 11:40:51 -0400

    Made nav and footer both responsive with text and tags. Have temp logos in the footer and navbar

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { AiOutlineInstagram, AiOutlineYoutube, AiOutlinePhone, AiOutlineMail, AiOutlineFacebook } from 'react-icons/ai';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import '../footer/'
+import '../footer/footer.css'
+import DanceLogo from '../../assets/P-logos_black.png';
 
 const Footer = () => {
 
     return(
         <>
-        
+            <section className="holds-everything-footer">
+                <div className="holds-footer-img">
+                    <img alt="footer-logo" src={DanceLogo} className='footer-logo'></img>
+                    <p className="small-sub-text">@Onpointe Dance Academy</p>
+                </div>
+                <div className="footer-social-media">
+                    <p>Check us out on...</p>
+                    <div className="social-dicons-row">
+                        <a href="/"><AiOutlineInstagram className="footer-social-icons"/></a>
+                        <a href="/"><AiOutlineYoutube className="footer-social-icons"/></a>
+                        <a href="/"><AiOutlineFacebook className="footer-social-icons"/></a>
+                    </div>
+                </div>
+                <div className="small-contact-info">
+                    <div className="phone-footer-contact">
+                        <AiOutlinePhone className="footer-contact-icons"/>
+                        <div className="div-padding-verysmall"></div>
+                        <a href="tel:3333333333"><p>407-888-5687</p></a>
+                    </div>
+                    <div className="mail-footer-contact">
+                        <AiOutlineMail className="footer-contact-icons"/>
+                        <div className="div-padding-verysmall"></div>
+                        <a href="mailto:something@gmail.com"><p>something@gmail.com</p></a>
+                    </div>
+                    
+                    
+                </div>
+            </section>
         </>
     )
 }
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import '../nav/'
+import '../nav/nav.css'
+import DanceLogo from '../../assets/P-logos_black.png';
 
 const Nav = () => {
+    
 
-    return(
+    return (
         <>
-            <p>Testing</p>
+            <section className="hold-everything-navbar">
+                <div className="holds-logo">
+                    <img alt='logo' className="navbar-logo" src={DanceLogo}></img>
+                </div>
+                <div className="holds-page-options">
+                    <a href='/' className="navbar-a-element"><h4>About Us</h4></a>
+                    <div className="div-padding-1"></div>
+                    <a href='/' className="navbar-a-element"><h4>Classes/Registration</h4></a>
+                    <div className="div-padding-1"></div>
+                    <a href='/' className="navbar-a-element"><h4>Contact Me</h4></a>
+                </div>
+                <div className="holds-page-options-mobile">
+
+                
+                </div>
+            </section>
         </>
     )
 }
