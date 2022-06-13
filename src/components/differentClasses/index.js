f8d3efd614c17da15f7aa4b6e9377f86d0b9cc06
Phillip48 <phillippereira02@gmail.com>
2022-06-13 18:35:24 -0400

    Added some needed css in all css files and added more startcode/frame code for the different classes offered

---
+++ src/components/differentClasses/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import '../differentClasses/classes.css'
+import TempPic from '../../assets/placeholder img.webp';
+
+const Classes = () => {
+
+    return (
+        <>
+            {/* Different classes offered */}
+            <section className="">
+                 {/* class info */}
+                <section className="">
+
+                    <div className="">  
+
+                    </div>
+
+
+                    {/* schedule */}
+                    <div className="">
+                        
+                    </div>
+                </section>
+
+                <div className="">
+                    <img alt='header-img' src={TempPic} className=''></img>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Classes;
