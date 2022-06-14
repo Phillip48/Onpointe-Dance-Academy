163fbd0dc2579698392ca3031893d840a46ab41a
Phillip48 <phillippereira02@gmail.com>
2022-06-14 10:20:25 -0400

    Added very basic frame and functions for the register form

--- src/components/registerForm/index.js
+++ src/components/registerForm/index.js
@@ -1,13 +1,33 @@
 import React from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import '../registerForm/registerForm.css'
-import TempPic from '../../assets/placeholder img.webp';
+// import TempPic from '../../assets/placeholder img.webp';
+import emailjs from '@emailjs/browser';
 
 const RegisterForm = () => {
+    const form = useRef();
+
+    let message = "Thanks for registering! I'll contact you soon!";
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        // Make a new template for the registeration form
+        emailjs.sendForm('service_30g88yk', 'template_hocpn0h', form.current, '3-bRDlrNfQF67KWz3')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        alert(message)
+    };
 
     return (
         <>
-            
+            <section className="holds-registeration-form">
+                <form  name='contact' className='register-form' ref={form} onSubmit={sendEmail}>
+
+                </form>
+            </section>
         </>
     )
 }
